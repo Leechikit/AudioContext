3d0340be404797a78ab7153d62db8174762f10e7
李子杰 <leechikit0823@163.com>
2017-05-12 10:25:22 +0800

    update rhythmSample.js

--- src/js/rhythmSample.js
+++ src/js/rhythmSample.js
@@ -19,10 +19,14 @@ function rhythmSample(obj) {
         function playSound(buffer, time) {
             var source = context.createBufferSource();
             source.buffer = buffer;
+            source.playbackRate.value = 1;
             source.connect(context.destination);
             source.start();
+            source.onended = () => {
+                console.log('end');
+            }
         }
-
+        playSound(BUFFERS.jam);
 
 
         // 立刻播放这段节奏
@@ -31,21 +35,21 @@ function rhythmSample(obj) {
         var eighthNoteTime = (60 / tempo) / 2;
 
         // 播放两个主音调:
-        for (var bar = 0; bar < 2; bar++) {
-            var time = startTime + bar * 8 * eighthNoteTime;
-            // 播放打击音1和5
-            playSound(kick, time);
-            playSound(kick, time + 4 * eighthNoteTime);
+        // for (var bar = 0; bar < 2; bar++) {
+        //     var time = startTime + bar * 8 * eighthNoteTime;
+        //     // 播放打击音1和5
+        //     playSound(kick, time);
+        //     playSound(kick, time + 4 * eighthNoteTime);
 
-            // 播放3，7
-            playSound(snare, time + 2 * eighthNoteTime);
-            playSound(snare, time + 6 * eighthNoteTime);
+        //     // 播放3，7
+        //     playSound(snare, time + 2 * eighthNoteTime);
+        //     playSound(snare, time + 6 * eighthNoteTime);
 
-            // 播放伴奏音.
-            for (var i = 0; i < 8; ++i) {
-                playSound(hihat, time + i * eighthNoteTime);
-            }
-        }
+        //     // 播放伴奏音.
+        //     for (var i = 0; i < 8; ++i) {
+        //         playSound(hihat, time + i * eighthNoteTime);
+        //     }
+        // }
     };
 
     function eventBind() {
