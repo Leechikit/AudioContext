3131326ce9a94f01a046538a1c461d8de6464e94
李子杰 <leechikit0823@163.com>
2017-08-02 16:49:50 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import Source from './js/createSource.js';
 
 setTimeout(() => {
-    var sound1 = new Source({
-        soundName: "drunk",
-        loop: true
-    });
-    var sound2 = new Source({
-        soundName: "眉飞色舞",
-        loop: true
-    });
+    // var sound1 = new Source({
+    //     soundName: "drunk",
+    //     loop: true
+    // });
+    // var sound2 = new Source({
+    //     soundName: "眉飞色舞",
+    //     loop: true
+    // });
     var sound3 = new Source({
         soundName: "可不可以不勇敢",
         loop: true
     });
 
-    sound1.start();
-    sound2.start();
+    // sound1.start();
+    // sound2.start();
     sound3.start();
+    setTimeout(()=>{
+        sound3.controlVolume(0.2);
+    },3000);
 }, 5000)
